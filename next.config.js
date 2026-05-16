6eb4f97a0edb2a3d8261d4fa1778b2937b0b4eb2
Kanu3690 <kanu3690@gmail.com>
2026-05-16 10:54:28 +0530

    config changed

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
- 
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: isProd ? "/car-dealership" : "",
-  assetPrefix: isProd ? "/car-dealership/" : "",
-};
- 
-export default nextConfig;
+  output: 'export'
+}
+module. Exports = nextConfig
