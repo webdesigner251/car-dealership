a073423662b11ddb547f0f938ebca279728d7a8e
Kanu3690 <kanu3690@gmail.com>
2026-05-18 11:49:33 +0530

    next config file update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 }
-module. Exports = nextConfig
+
+module.exports = nextConfig
